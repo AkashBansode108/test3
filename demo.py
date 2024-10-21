49b56337467b5885c395ae9eee9adee3bda556aa
akash bansode <bansodeakash108@gmail.com>
2024-10-21 23:18:10 +0530

    if else added

--- demo.py
+++ demo.py
@@ -2,3 +2,13 @@ def add(x,y):
     return x+y
 
 print(add(1,2))
+
+#these chnages are added in feature 2
+
+# this feature added in feature3
+
+if True:
+    print("True")
+else:
+    print("false")
+    
